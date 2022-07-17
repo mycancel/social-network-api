043edc74571274cc0ed2443079e34585d7713de0
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-17 17:14:28 -0400

    added userSchema, added friendCount virtual; initialized User model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+// Schema for what makes up user information
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: "/^([a-z0-9._-]+)@([a-z0-9.]+)$/gm",
+    },
+    thoughts: [thoughtSchema],
+    friends: [userSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Retrieves length of user's friends array field on query
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+// Initialize the User model
+const User = model("user", userSchema);
+
+module.exports = User;
