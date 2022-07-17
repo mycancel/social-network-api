c9df0df3f0e0b980bfe27753c58e4c24587e3604
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-17 17:16:32 -0400

    created thought schema subdocument

--- models/User.js
+++ models/User.js
@@ -1,5 +1,25 @@
 const { Schema, model } = require("mongoose");
 
+// Schema for thought subdocument
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now
+    // TODO: create getter method to format the timestamp
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+});
+
 // Schema for what makes up user information
 const userSchema = new Schema(
   {
