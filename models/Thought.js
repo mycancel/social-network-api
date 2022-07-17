011b836c2d2f2c231b91607ef986af65fad99edc
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-17 17:24:41 -0400

    removed Reaction.js require from models/index.js; added reactionSchema subdocument to Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,23 @@
 const { Schema, model } = require("mongoose");
 
+// Schema for reaction subdocument
+const reactionSchema = new Schema({
+  reactionBody:{
+    type: String,
+    required: true,
+    maxLength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date, 
+    default: Date.now
+    // TODO: create getter method to format the timestamp
+  }
+});
+
 // Schema for thought subdocument
 const thoughtSchema = new Schema({
   thoughtText: {
