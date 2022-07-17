171728645e3321f78fab9084bcea8dc583d0fc08
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-07-17 18:28:23 -0400

    added getUsers, getSingleUser, and createNewUser to userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,3 +5,33 @@
 // deleteUser,
 // addNewFriend,
 // deleteFriend
+
+const { User } = require('../models');
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user found with that id' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Create a user
+  createNewUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
+  },
+};
